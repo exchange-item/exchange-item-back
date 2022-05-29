0a1ca14690702f5685bf630751ab812dd792afd3
noparamin <noparamin@naver.com>
2022-05-30 00:49:30 +0900

    feat: 프로필

---
+++ src/main/java/com/bakooza/bakooza/Controller/LoginController.java
@@ -0,0 +1,72 @@
+package com.bakooza.bakooza.Controller;
+
+import com.bakooza.bakooza.Domain.Member;
+import com.bakooza.bakooza.Service.JwtUtils;
+import com.bakooza.bakooza.Service.MemberService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+
+@RestController
+@Slf4j
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class LoginController {
+
+    private final MemberService ms;
+    private final JwtUtils jwtUtils;
+
+    public LoginController(MemberService ms, JwtUtils jwtUtils) {
+        this.ms = ms;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @PostMapping("/oauth/kakao/login")
+    public ResponseEntity<Object> kakaoCallback(@RequestParam(value = "code") String code) throws JsonProcessingException, UnsupportedEncodingException {
+
+        log.info("authorizationCode={}", code);
+
+        String accessToken = ms.getAccessToken(code);
+        log.info("getAccessToken 진입 후 받아 온 accessToken = {}", accessToken);
+
+        HashMap<String, Object> userInfo = ms.getUserInfo(accessToken);
+
+        Member member = new Member();
+        member.setId(Long.parseLong(userInfo.get("id").toString())-10000);
+        member.setNickname(userInfo.get("nickname").toString());
+        member.setEmail(userInfo.get("email").toString());
+        member.setProfileImage(userInfo.get("profileImage").toString());
+
+        Boolean isMemberPresent = ms.findMemberById(member.getId()).isPresent();
+
+        if(isMemberPresent == false) {
+            ms.join(member);
+        }
+        else {
+            log.info("The member is already sign up");
+        }
+
+        String jwtAccessToken = jwtUtils.createJwt(member);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("jwtAccessToken", jwtAccessToken);
+        headers.set("nickname", member.getNickname());
+        headers.set("email", member.getEmail());
+
+        log.info("headers = {}", headers);
+        log.info("headers jwtAccessToken = {}", headers.get("jwtAccessToken"));
+
+        return new ResponseEntity<>("로그인 성공", headers, HttpStatus.OK);
+    }
+
+    @GetMapping("hi")
+    public ResponseEntity<Object> Hello() {
+        return new ResponseEntity<>("hi~~", HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/bakooza/bakooza/Repository/MemberRepository.java
@@ -0,0 +1,55 @@
+package com.bakooza.bakooza.Repository;
+
+import com.bakooza.bakooza.Domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class MemberRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public Member save(Member member) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member");
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("member_id", member.getId());
+        parameters.put("nickname", member.getNickname());
+        parameters.put("email", member.getEmail());
+        parameters.put("profile_image", member.getProfileImage());
+
+        jdbcInsert.execute(parameters);
+
+        return member;
+    }
+
+    public Optional<Member> findById(Long id) {
+        List<Member> result = jdbcTemplate.query("select * from member where member_id = ?", memberRowMapper(), id);
+        return result.stream().findAny();
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("member_id"));
+            member.setNickname(rs.getString("nickname"));
+            member.setEmail(rs.getString("email"));
+            member.setProfileImage(rs.getString("profile_image"));
+
+            return member;
+        };
+    }
+}
